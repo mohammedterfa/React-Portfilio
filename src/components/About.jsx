4aa45cd4883c57688e50e6932c1e341dba80779a
mohammedterfa <mohammedterfa@gmail.com>
2023-05-06 20:53:06 +0300

    starting about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.dev variants={textVariant()}>
+        <p className={styles.sectionSubText} >Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.dev>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+
+        I'm a skilled software developer with Experience in typeScript and JavaScript, and
+        expertise in frameworks like React, Node.js, and collaborate closely with clients to create efficient, scalable, and 
+        user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life!
+
+
+      </motion.p>
+    </>
   )
 }
 
