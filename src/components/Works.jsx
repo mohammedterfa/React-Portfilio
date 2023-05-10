742cc4dc1b5878938a09f148a4499bf6c609174a
mohammedterfa <mohammedterfa@gmail.com>
2023-05-10 20:32:11 +0300

    code

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -6,7 +6,13 @@ import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
-const  ProjectCard = () =>{}
+const  ProjectCard = ({ index, name, description, tags, image, source_code_link }) =>{
+  return (
+    <motion.div>
+      test
+    </motion.div>
+  )
+}
 
 const Works = () => {
   return (
