7ac65b278f3405fa864f14a756e352510aff50d5
mohammedterfa <mohammedterfa@gmail.com>
2023-05-11 10:13:42 +0300

    code

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -5,7 +5,14 @@ import { fadeIn, textVariant } from "../utils/motion";
 import { testimonials } from "../constants";
 
 
-
+const FeedbackCard = ({index, testimonial, name, designation, company, image}) => (
+  <motion.div 
+    variants={fadeIn("", "spring", index * 0.5, 0.75 )}
+    className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full"
+  >
+    <p>{testimonial}</p>
+  </motion.div>
+);
 
 const Feedbacks = () => {
   return (
