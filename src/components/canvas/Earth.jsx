d852aab494efbab4860a43fc9260837e5228cc02
mohammedterfa <mohammedterfa@gmail.com>
2023-05-13 18:19:44 +0300

    code

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -4,8 +4,15 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 const Earth = () => {
+
+  const earth = useGLTF('./planet/scene.gltf');
   return (
-    <div>Earth</div>
+    <primitive 
+      object={earth.scene}
+      scale={2.5}
+      position-y={0}
+      rotation-y={0}
+    />
   )
 }
 
@@ -15,7 +22,12 @@ const EarthCanvas = () => {
       shadows
       frameloop='demand'
       gl={{ preserveDrawingBuffer: true }}
-      camera={{}}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6]
+      }}
     >
       <Suspense fallback={<CanvasLoader/>}>
         <OrbitControls 
