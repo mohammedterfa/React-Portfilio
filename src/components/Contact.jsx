22ef09d936f10a4d85d107cd5cbd1c31075358ab
mohammedterfa <mohammedterfa@gmail.com>
2023-05-13 12:31:18 +0300

    code

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ import  { slideIn } from '../utils/motion';
 
 const Contact = () => {
   const formRef = useRef();
-  const [form, setForm] = userState({
+  const [form, setForm] = useState({
     name: '',
     email: '',
     message: '',
@@ -17,15 +17,27 @@ const Contact = () => {
 
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) =>{};
+  const handleChange = (e) => {}
 
-  const handleSubmit = (e) => {};
-
-
+  const handleSubmit = (e) => {}
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn('left', "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      >
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
 
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+
+        </form>
+      </motion.div>
     </div>
   )
 }
